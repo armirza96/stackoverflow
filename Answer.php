6cc318aeb7976c8656a3bd0c753977065422306d
haytham5 <haythamhnine5@gmail.com>
2021-09-22 14:32:14 -0400

    Initial Commits of Work item 3
    Commits of static versions of the Answer page, and navigation bar header file
    
    Renaming old index file so opening "localhost" doesn't open it automatically

---
+++ Answer.php
@@ -0,0 +1,93 @@
+<?php include("header.php"); ?>
+
+
+<body>
+<link rel="stylesheet" href="css/Answer.css">
+<br>
+<div class="main">
+
+    <div id="question-container">
+        <table border="0" width="100%" id="question-pane">
+            <col style="width:5%"></col>
+            <col style="width:95%"></col>
+
+            <tr>
+                <td style="text-align: center">
+                    <button type="button" class="upvote">▲</button>
+                </td>
+
+                <td rowspan="3" style="padding-top:15px">        
+                    <p id="question">How do I program?</p>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: center; font-size: 20px">1</td>
+            </tr>
+            <tr>
+                <td style="text-align: center">
+                    <button type="button" class="downvote">▼</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div id="question-explanation-container">
+        <p id="question-explanation">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+        mollit anim id est laborum.
+        </p>
+    </div>
+</div>
+
+<br>
+
+<div class="main">
+    <div id="answer-box-container">
+        <p id="answer-box-name">Your Answer</p>
+
+        <div class="text-buttons">
+            <button class="button"><strong>B</strong></button>
+            <button class="button"><em>I</em></button>
+            <button class="button">IMG</button>
+            <button class="button">list</button>
+        </div>
+
+        <textarea id="answer-box" rows="8" cols="50" placeholder="Type your answer here..."></textarea>
+    </div>
+
+    <div id="answers-container">
+        <table border="0" width="100%" id="answer-pane">
+            <col style="width:5%"></col>
+            <col style="width:95%"></col>
+
+            <tr>
+                <td style="text-align: center">
+                    <button type="button" class="upvote">▲</button>
+                </td>
+
+                <td rowspan="3" class="answer-row">        
+                    <p class="answer">Type "Hello World."</p>
+                </td>
+            </tr>
+            <tr>
+                <td style="text-align: center; font-size: 20px">1</td>
+            </tr>
+            <tr>
+                <td style="text-align: center">
+                    <button type="button" class="downvote">▼</button>
+                </td>
+            </tr>
+        </table>
+
+        <br>
+    </div>
+</div>
+
+<br>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/header.css">
+
+    <div class="topnav">
+        <a class="active" href="#home">Slack Overflow</a>
+        <a href="#about">About</a>
+        <a href="#login">Log in</a>
+        <a href="#signup">Sign up</a>
+        <a href="#ask">Ask a Question</a>
+
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="Search..." name="search">
+                <button type="submit"><i class="fa fa-search">Submit</i></button>
+            </form>
+        </div>
+    </div>
+</head>
