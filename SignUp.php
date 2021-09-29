b0ade90227ac8229aa3e87cda1bb6d7f55323f1c
Najeeb Hyatoolla <najeebhayat@hotmail.com>
2021-09-29 17:51:43 -0400

    modified SignUp
    Added Email text box

--- SignUp.php
+++ SignUp.php
@@ -6,9 +6,11 @@
     <h1 style="text-align:center;">Sign Up Page</h1>
 <div class="main" style="text-align:center">
     <form action="SignUp.php">
-        <label for="username"> Username:</label>
+        <label for="username"> <b>Username</b></label><br>
         <input type="text" id="username" name="username"><br><br>
-        <label for="password"> Password:</label>
+        <label for="email"> <b>Email</b></label><br>
+        <input type="text" id="email" name="email"><br><br>
+        <label for="password"> <b>Password</b></label><br>
         <input type="text" id="password" name="password"><br><br>
         <input type="submit" value="SignUp" style="width: 100px;">
     </form>
