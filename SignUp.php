6f7274700c722a092fcb3de468550f51f80ef51b
NajeebH <90568747+NajeebH@users.noreply.github.com>
2021-10-15 21:35:17 -0400

    Modified SignUp.php & signup.css

--- SignUp.php
+++ SignUp.php
@@ -1,10 +1,13 @@
 <?php include("header.php"); ?>
 
+<head>
+   <link rel="stylesheet" href="css\signup.css"> 
+</head>
+
 <body>
-<link rel="stylesheet" href="css/signup.css"> 
-<br>
-    <h1 style="text-align:center;">Sign Up Page</h1>
-<div class="main" style="text-align:center">
+    <h1>Sign Up Page</h1>
+<div class="main">
+    <div id="box-content">
     <form action="SignUp.php">
         <label for="username"> <b>Username</b></label><br>
         <input type="text" id="username" name="username"><br><br>
@@ -12,8 +15,12 @@
         <input type="text" id="email" name="email"><br><br>
         <label for="password"> <b>Password</b></label><br>
         <input type="text" id="password" name="password"><br><br>
-        <input type="submit" value="SignUp" style="width: 100px;">
+        <input type="submit" value="Sign Up">
     </form>
+    </div>
 </div>
+
+<p>Already have an account? <a href="Login.php">Log in</a></p>
+
 </body>
 </html>
