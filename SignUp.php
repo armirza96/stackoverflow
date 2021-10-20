74b577d9f287296debb33c4ebcaba1b3a958bb76
NajeebH <90568747+NajeebH@users.noreply.github.com>
2021-10-20 18:18:35 -0400

    Modified SignUp.php using Bootstrap

--- SignUp.php
+++ SignUp.php
@@ -1,26 +1,68 @@
-<?php include("header.php"); ?>
+<?php
+$title = "Sign Up - Stack Overflow";
 
-<head>
-   <link rel="stylesheet" href="css\signup.css"> 
-</head>
+include("header.php"); ?>
+
+<!-- <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head> -->
 
 <body>
-    <h1>Sign Up Page</h1>
-<div class="main">
+<link rel="stylesheet" href="css/signup.css">
+  <div class="jumbotron text-center">
+      <h1>Create Your Account</h1>
+  </div>
+
+  <div class="d-flex justify-content-center">
+
+  <div class="alert alert-success hide" role="alert" id="alert-success">
+    This is a success alert—check it out!
+  </div>
+  <div class="alert alert-danger hide" role="alert" id="alert-danger">
+    This is a danger alert—check it out!
+  </div>
+
     <div id="box-content">
-    <form action="SignUp.php">
-        <label for="username"> <b>Username</b></label><br>
-        <input type="text" id="username" name="username"><br><br>
-        <label for="email"> <b>Email</b></label><br>
-        <input type="text" id="email" name="email"><br><br>
-        <label for="password"> <b>Password</b></label><br>
-        <input type="text" id="password" name="password"><br><br>
-        <input type="submit" value="Sign Up">
+    <form action="user/signup" id="signup">
+      <div class="form-group">
+        <label for="username"><b>Username</b></label>
+        <input type="text" class="form-control" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="email"><b>Email</b></label>
+        <input type="text" class="form-control" id="email" name="email" required>
+      </div>
+      <div class="form-group">
+        <label for="password"><b>Password</b></label>
+        <input type="password" class="form-control" id="password" name="password" required>
+      </div>
+      <br>
+      <button id="button" onclick='submitForm("signup", "onDataReceived")'>Submit</button>
     </form>
     </div>
-</div>
+  </div>
 
-<p>Already have an account? <a href="Login.php">Log in</a></p>
+  <br>
+  <div class="d-flex justify-content-center">
+  <p>Already have an account? <a href="Login.php">Log in</a></p>
+  </div>
 
+<script src="shared/js/shared.js"></script>
+<script>
+
+  function  onDataReceived(data) {
+    if(data.RESULT == 1) {
+      showAlert("alert-success",data.MESSAGE + '. ' + 'Redirecting in 3 seconds');
+      redirectUser(`profile.php?ID=${data.ID}`,3000);
+    } else {
+      showAlert("alert-danger", data.MESSAGE);
+    }
+  }
+</script>
 </body>
 </html>
