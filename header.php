b135ae4eaaaedf3d099ac64438a3a559d635bb55
NajeebH <90568747+NajeebH@users.noreply.github.com>
2021-11-02 21:32:01 -0400

    Modified Navbar using Bootstrap
    Made the navbar more responsive. Still missing the search bar for now.

--- header.php
+++ header.php
@@ -8,21 +8,60 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="css/main.css">
 
     <title><?=$title ?></title>
 </head>
 
 <body>
-<div class="topnav">
+<!-- <div class="topnav">
     <a class="active" href="main.php">Slack Overflow</a>
-    <!--<a href="#about">About</a>-->
+    <!- <a href="#about">About</a> ->
     <a href="Login.php">Log in</a>
     <a href="SignUp.php">Sign up</a>
     <a href="Questions-Page.php">Ask a Question</a>
     <a href="edit-profile.php">Check User Profile</a>
     <a href="Answer.php">Answer page (Temp)</a>
 
-    <!-- <form class="d-flex m-2 "> <input class="form-control mr-2" type="search" placeholder="Search..." aria-label="Search"> <button class="btn btn-outline-light" type="submit">Search</button> </form> -->
+    <!- <form class="d-flex m-2 "> <input class="form-control mr-2" type="search" placeholder="Search..." aria-label="Search"> <button class="btn btn-outline-light" type="submit">Search</button> </form> ->
 
-</div>
+</div> -->
+
+<div class="topnav">
+<nav class="navbar navbar-expand-lg navbar-light">
+    
+  <div class="container-fluid">
+    <a class="navbar-brand" href="main.php" style="color: white;">Stack Overflow</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="SignUp.php">Sign Up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="Questions-Page.php">Ask a Question</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="edit-profile.php">Check User Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="Answer.php">Answer page (Temp)</a>
+        </li>
+      </ul>
+    </div>
+
+  </div>
+  </div>
+</nav>
+
+</body>
+</html>
